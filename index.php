9f57bbcc1ecfd809fb93f4620d3afe99c36b3433
Rob Tognoni <60898339+rtogn@users.noreply.github.com>
2022-04-29 16:26:33 -0400

    Add files via upload
    Features:
    -Added login functionality to index. Will rout you to admin.php, seller.php or buyer.php based on your user type.
       see Server_Connection.php for the SQL query (function is verifyUser().
    -Added skeletons for buyer and admin dashboards. They are copies of the seller skeleton with the titles changed. 
    
    Username and passwords for dummy entries to test functions:
    Seller- test, test
    Buyer- testbuy, testbuy
    I have not added an Admin yet.

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>  
  
  <?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
 	include("Server_Connection.php");
 	//start session
+	session_destroy();
 	session_start();
+	
 	$currentUser = -1;
 	if (isset($_SESSION['username'])) {
 		$currentUser = $_SESSION['username'];
+	}
+	
+	if (isset($_POST["password"]) && strlen($_POST["password"]) > 0) {
+		verifyUser($_POST["username"], $_POST["password"]);
 	}
  ?>
  
@@ -25,18 +34,11 @@
                 <img src="./images/houseLogo.png">
             </div>
             <h1 class="login"> Login </h1>
-            <input name="username" type="text" placeholder="Username" id="username">
-            <input name="password" type="password" placeholder="Password">
+            <input name="username" type="text" placeholder="Username" id="username" required>
+            <input name="password" type="password" placeholder="Password" required>
             <input class="loginButton" type="submit" value="LOGIN">
             <a href="./signUp.php" class="signUpButton">SIGN UP</a>
         </form>
-		
-	 <script>
-		// This is so hackey crap to fill out the user name field if its already in the session. 
-		var fill_usr = <?php echo $currentUser;?>;
-		if (fill_usr != -1) {
-			document.getElementById('username').value = fill_usr;
-		}
-	 </script>		
+			
     </body> 
 </html>
