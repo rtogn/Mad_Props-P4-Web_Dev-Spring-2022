09435b9e5246eade5f618f23e704ed93b139e675
Brett Krokoff <55294152+Bkrokoff@users.noreply.github.com>
2022-04-28 21:40:57 -0400

    JS for radio Buttons

--- signUp.js
+++ signUp.js
@@ -1,20 +1,16 @@
 // Got help from https://github.com/solodev/hide-show-fields-form/blob/master/hide-show-fields-form.js 
 var type = document.getElementById("type");
 
+document.getElementById("buyers").onclick = function () {
+	document.getElementById("creditInfo").style.display = "block";
+	document.getElementById("creditcard").setAttribute('required', '');
+	document.getElementById("creditType").setAttribute('required', '');
+	document.getElementById("securityCode").setAttribute('required', '');
+};
 
-// Hides and shows CC info based on what option is selected. 
-type.addEventListener('change', function() {
-		if(type.value == "seller" || type.value == "admin") {
-			document.getElementById("creditInfo").style.display = "none";
-			document.getElementById("creditcard").removeAttribute('required');
-			document.getElementById("creditType").removeAttribute('required');
-			document.getElementById("securityCode").removeAttribute('required');
-		}
-		if (type.value == "buyer") {
-			document.getElementById("creditInfo").style.display = "block";
-			document.getElementById("creditcard").setAttribute('required', '');
-			document.getElementById("creditType").setAttribute('required', '');
-			document.getElementById("securityCode").setAttribute('required', '');
-		}
-	});
-	//document.getElementById("type").trigger("change");
+document.getElementById("sellers").onclick = function () {
+	document.getElementById("creditInfo").style.display = "none";
+	document.getElementById("creditcard").removeAttribute('required');
+	document.getElementById("creditType").removeAttribute('required');
+	document.getElementById("securityCode").removeAttribute('required');
+};
