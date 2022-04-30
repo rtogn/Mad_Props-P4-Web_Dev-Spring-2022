5d329aa4d89c623fdd7d3787d1cec5c9779b9da2
Brett Krokoff <55294152+Bkrokoff@users.noreply.github.com>
2022-04-30 14:43:52 -0400

    Recreated Card

--- buyer.php
+++ buyer.php
@@ -14,13 +14,14 @@
     //Code to pull user properties from database
     //If properties are 0, display no filled cards
     ?>
-      <h2>New Property</h2>
-      <p>
-        Add Property
-      </p>
-      <a href="addProperty.php" class="main"></a>
-      <button id="new">+</button>
-    </li>
+    
+    <a href="addProperty.php" class="">
+    <div class="card">
+      <img src="./images/AddProperty.jpg">
+      <div class="contain">
+        <h4>Add Property</h4>
+    </div>
+    </a>
   </ul>
 </body>
 </html>
