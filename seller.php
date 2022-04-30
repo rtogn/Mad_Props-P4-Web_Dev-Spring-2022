869361497110b547826762f03a98ff8d2907db65
rtogn <rtognoni1@student.gsu.edu>
2022-04-30 14:31:37 -0400

    -_- I made a dumb with git heres the fix.

--- seller.php
+++ seller.php
@@ -12,49 +12,46 @@
   <ul class="fullclick">
   <?php
 
-	session_start();
-	/*
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
-	*/
-	
-	include("SQL_Functions.php");
-	$conn = getConn();
+		session_start();
+		
+		/*
+		ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
+		*/
+		
+		include("SQL_Functions.php");
+		$conn = getConn();
 
-	
-    if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-    }
-   
-    $sql = "SELECT 
-				title, 
-				address1, 
-				value
-			FROM PROPERTIES 
-			WHERE 
-			ownerID = '".$_SESSION['userId']."'
-			;";
-			
-    $result = $conn->query($sql);
-    if ($result->num_rows > 0) {
-      // output data of each row
-    while($row = $result->fetch_assoc()) {
-     echo "<li><p>" . $row["title"] . "</p><p>" . $row["address1"] . "</p><p>" . $row["value"] . "</p>";
-     ?>
-    <form action="property.php" class="main">
-       <input type="text" name="propID" value=<?$row["id"]?> readonly>
-       <input type="submit" value="Go to property" class="main">
-    </form>
-     <?php
-     echo "</li>"
-    }
-   //  Run a loop and display the records on screen dynamically
-   // lets say the above query returned 20 rows
-   // Now display the table on screen with 20 records
-
-    }
-    $conn->close();
+		
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+	   
+		$sql = "SELECT 
+					title, 
+					address1, 
+					value
+				FROM PROPERTIES 
+				WHERE 
+				ownerID = '".$_SESSION['userId']."'
+				;";
+				
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+		  // output data of each row
+			//  Run a loop and display the records on screen dynamically
+			// lets say the above query returned 20 rows
+			// Now display the table on screen with 20 records
+			while($row = $result->fetch_assoc()) {
+				echo "<li><p>" . $row["title"] . "</p><p>" . $row["address1"] . "</p><p>" . $row["value"] . "</p>";
+				echo "<form action=\"property.php\" class=\"main\">";
+				echo " <input type=\"submit\" value=\"Go to property\" class=\"main\">";
+				echo "</form>";
+				echo "</li>";
+			}
+		}
+		$conn->close();
     ?>
     <li>
       <h2>New Property</h2>
